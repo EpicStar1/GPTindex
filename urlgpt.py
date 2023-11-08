9bd444997db549e237bb79343f22e9a1bfb868b8
Amit kumar Pandey <Amitkumar.Pandey@rockwellautomation.com>
2023-11-08 10:30:09 +0530

    Change in urlgpt functionality

--- urlgpt.py
+++ urlgpt.py
@@ -4,6 +4,9 @@ import sys
 import os
 import constants
 import requests
+from urllib.parse import urlsplit
+import validators
+import tldextract
 from langchain.chat_models import ChatOpenAI
 from langchain.document_loaders import WebBaseLoader
 from langchain.indexes import VectorstoreIndexCreator
@@ -12,32 +15,19 @@ from bs4 import BeautifulSoup
 
 os.environ["OPENAI_API_KEY"] = constants.APIKEY
 
-def chatbot(input_text):
-    #url = 'https://www.plex.com/smart-manufacturing-platform'
-
-    #urls = getchildurl(url)
-
-    urls = [
-        'https://www.plex.com/smart-manufacturing-platform',
-        'https://www.plex.com/products/manufacturing-execution-system',
-        'https://www.plex.com/industries/food-and-beverage/why-mes-critical-food-and-beverage-manufacturers'
-    ]
-    
-    loader = WebBaseLoader(urls)
-    
-    index = VectorstoreIndexCreator().from_loaders([loader])
-
+def chatbot(query): 
     chain = ConversationalRetrievalChain.from_llm(
         llm=ChatOpenAI(model="gpt-3.5-turbo", temperature="0.7"),
         retriever=index.vectorstore.as_retriever(search_kwargs={"k": 1}),
     )
 
     chat_history = []
-    result = chain({"question": input_text, "chat_history": chat_history}) 
-    chat_history.append((input_text, result['answer']))   
+    result = chain({"question": query, "chat_history": chat_history}) 
+    chat_history.append((query, result['answer']))  
+
     return result['answer']
 
-
+# Get all child url present on page
 def getchildurl (url):
     base_add = 'https://www.plex.com'
     reqs = requests.get(url)
@@ -46,16 +36,24 @@ def getchildurl (url):
     urls = []
     for link in soup.find_all('a'):
         url = link.get('href')
-        if (url != None and url != '' and url != '#'):
+        if (url != None and url != '' and url != '#'):            
             if "https:" in url:
-                urls.append(url)
+                if is_valid_url(url):
+                    urls.append(url)
             else:
                 url = base_add + url
-                urls.append(url)
-    
+                if is_valid_url(url):
+                    urls.append(url)   
     return urls
 
 
+# Check valid URL
+def is_valid_url(url):
+    extracted = tldextract.extract(url)
+    return validators.url(url) and bool(extracted.domain) and bool(extracted.suffix)
+
+
+# Gradio interface for user input
 iface = gr.Interface(fn=chatbot,
                      inputs=gr.components.Textbox(lines=7, label="Enter your query"),
                      outputs="text",
@@ -63,4 +61,22 @@ iface = gr.Interface(fn=chatbot,
                      allow_flagging='never',
                      flagging_options=None)
 
-iface.launch(share=True)
+if __name__ == '__main__':    
+    # url = 'https://www.plex.com/smart-manufacturing-platform'
+    
+    urls = [
+        'https://www.plex.com/smart-manufacturing-platform',
+        'https://www.plex.com/products/manufacturing-execution-system',
+        'https://www.plex.com/industries/food-and-beverage/why-mes-critical-food-and-beverage-manufacturers'
+    ]
+
+    uniqueUrl = set()
+    for url in urls:
+        uniqueUrl.update(getchildurl(url))
+
+    childUrls = list(uniqueUrl)
+    
+    loader = WebBaseLoader(childUrls)    
+    index = VectorstoreIndexCreator().from_loaders([loader])
+
+    iface.launch(share=True)
